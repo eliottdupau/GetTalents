e76f05f7aba16810ea8577c02fa235723bfe856c
Eliott Dupau <eliott.dupau@gmail.com>
2021-05-03 15:16:36 +0200

    Init Project

---
+++ app/src/main/java/com/eliottdup/gettalents/ui/map/MapFragment.java
@@ -0,0 +1,31 @@
+package com.eliottdup.gettalents.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.eliottdup.gettalents.R;
+
+public class MapFragment extends Fragment {
+
+    public MapFragment() {}
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/eliottdup/gettalents/ui/menu/MenuFragment.java
@@ -0,0 +1,33 @@
+package com.eliottdup.gettalents.ui.menu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eliottdup.gettalents.R;
+
+public class MenuFragment extends Fragment {
+
+    public MenuFragment() { }
+
+    public static MenuFragment newInstance() {
+        return new MenuFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        return root;
+    }
+}
