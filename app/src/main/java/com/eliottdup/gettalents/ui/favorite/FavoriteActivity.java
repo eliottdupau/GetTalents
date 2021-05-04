355a9c823346a8551deeafe87a4e0f6cced011d4
Eliott Dupau <eliott.dupau@gmail.com>
2021-05-04 16:53:27 +0200

    feat(menu): Add new menu
    Create menu (v1) in menu tab & configure navigation

---
+++ app/src/main/java/com/eliottdup/gettalents/ui/favorite/FavoriteActivity.java
@@ -0,0 +1,16 @@
+package com.eliottdup.gettalents.ui.favorite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eliottdup.gettalents.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
--- app/src/main/java/com/eliottdup/gettalents/ui/menu/MenuFragment.java
+++ app/src/main/java/com/eliottdup/gettalents/ui/menu/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.eliottdup.gettalents.ui.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.eliottdup.gettalents.R;
+import com.eliottdup.gettalents.ui.authentication.AuthenticationActivity;
+import com.eliottdup.gettalents.ui.favorite.FavoriteActivity;
+import com.eliottdup.gettalents.ui.profile.ProfileActivity;
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuFragment extends Fragment {
+    private MaterialCardView profileItem, favoriteItem, logoutItem;
 
     public MenuFragment() { }
 
@@ -28,6 +34,31 @@ public class MenuFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_menu, container, false);
 
+        profileItem = root.findViewById(R.id.cardView_profile);
+        favoriteItem = root.findViewById(R.id.cardView_favorite);
+        logoutItem = root.findViewById(R.id.cardView_logout);
+
+        setupItemClick(profileItem);
+        setupItemClick(favoriteItem);
+        setupItemClick(logoutItem);
+
         return root;
+    }
+
+    private void setupItemClick(View view) {
+        view.setOnClickListener(v -> startActivity(getIntent(view)));
+    }
+
+    private Intent getIntent(View view) {
+        switch (view.getId()) {
+            case R.id.cardView_profile:
+                return new Intent(getContext(), ProfileActivity.class);
+            case R.id.cardView_favorite:
+                return new Intent(getContext(), FavoriteActivity.class);
+            case R.id.cardView_logout:
+                return new Intent(getContext(), AuthenticationActivity.class);
+            default:
+                return new Intent();
+        }
     }
 }
